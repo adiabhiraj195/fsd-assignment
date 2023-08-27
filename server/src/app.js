06b55949f3e2e6e30bcb4987e227bcca73c83107
aditya <adiabhiraj141@gmail.com>
2023-08-28 02:52:32 +0530

    .

--- server/src/app.js
+++ server/src/app.js
@@ -5,11 +5,12 @@ const morgan = require("morgan");
 const router = require("./router/index.js");
 
 const app = express();
+const BASE_URL = process.env.BASE_URL;
 
 app.use(morgan("combined"));
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: `${BASE_URL}`,
 }));
 
 app.use(router);
